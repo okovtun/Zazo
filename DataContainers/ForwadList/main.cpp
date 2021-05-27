ed19151fb0e4069e1e605c7ccc767790a86c4b52
Clayman <Clayman@DESKTOP-DM8E77U>
2021-05-27 13:49:26 +0300

    ForwardList day 2

--- DataContainers/ForwadList/main.cpp
+++ DataContainers/ForwadList/main.cpp
@@ -2,32 +2,41 @@
 using namespace std;
 
 #define tab "\t"
+#define delimiter "\n-----------------------------------------------------------\n"
 
 class Element
 {
 	int Data;	//Значение элемента
 	Element* pNext;	//Адрес следующего элемента
+	static int count;	//Количество элементов
 public:
 	Element(int Data, Element* pNext = nullptr)
 	{
+		count++;
 		this->Data = Data;
 		this->pNext = pNext;
 		cout << "EConstructor:\t" << this << endl;
 	}
 	~Element()
 	{
+		count--;
 		cout << "EDestructor:\t" << this << endl;
 	}
 	friend class ForwardList;
 };
 
+int Element::count = 0;	//Инициализация статической переменной
+//:: - Оператор разрешения видимости
+
 class ForwardList
 {
 	Element* Head;
+	unsigned int size;
 public:
 	ForwardList()
 	{
 		this->Head = nullptr;//Если Голова указывает на 0, значит список пуст
+		this->size = 0;	
 		cout << "LConstructor:\t" << this << endl;
 	}
 	~ForwardList()
@@ -41,9 +50,15 @@ public:
 		Element* New = new Element(Data);
 		New->pNext = Head;
 		Head = New;
+		size++;
 	}
 	void push_back(int Data)
 	{
+		if (Head == nullptr)
+		{
+			push_front(Data);
+			return;
+		}
 		Element* New = new Element(Data);
 		Element* Temp = Head;
 		while (Temp->pNext)
@@ -51,7 +66,52 @@ public:
 			Temp = Temp->pNext;
 		}//После этого цикла мы оказались в последнем элементе
 		Temp->pNext = New;
+		size++;
 	}
+	void insert(int index, int data)
+	{
+		//0) Проверяем индекс:
+		if (index > Element::count)return;
+		//Ключевое слово return ПРЕРЫВАЕТ РАБОТУ ФУНКЦИИ и возвращает управление на место вызова.
+
+		if (index == 0)
+		{
+			push_front(data);
+			return;
+		}
+
+		//1) Доходим до нужного элемента:
+		Element* Temp = Head;
+		for (int i = 0; i < index - 1; i++)
+			Temp = Temp->pNext;
+		//2) Содаем новый элемент, и добавляем его в список:
+		Element* New = new Element(data);
+		New->pNext = Temp->pNext;
+		Temp->pNext = New;
+		size++;
+	}
+
+	//				Removing elements:
+	void pop_front()
+	{
+		//0)Запоминаем адрес удаляемого элемента:
+		Element* to_del = Head;
+		//1) Исключить элемент из списка:
+		Head = Head->pNext;
+		//2) Удалить элемент и памяти:
+		delete to_del;
+		size--;
+	}
+	void pop_back()
+	{
+		Element* Temp = Head;
+		while (Temp->pNext->pNext)	//Доходим до предпоследнего элемента списка
+			Temp = Temp->pNext;
+		delete Temp->pNext;
+		Temp->pNext = nullptr;
+		size--;
+	}
+	
 
 	//				Methods:
 	void print()
@@ -64,8 +124,13 @@ public:
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext;	//Переход на следующий элемент
 		}
+		cout << "Количество элементов списка: " << size << endl;
+		cout << "Общее колчество элементов:   " << Element::count << endl;
 	}
 };
+
+//#define BASE_CHECK
+//define - определить
 
 void main()
 {
@@ -78,6 +143,29 @@ void main()
 		list.push_front(rand() % 100);
 	}
 	list.print();
+
+#if defined BASE_CHECK
+	cout << delimiter << endl;
 	list.push_back(123);
 	list.print();
+	cout << delimiter << endl;
+	list.pop_front();
+	list.print();
+	cout << delimiter << endl;
+	list.pop_back();
+	list.print();
+	cout << delimiter << endl;
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	list.insert(index, value);
+	list.print();
+#endif // BASE_CHECK
+
+	ForwardList list2;
+	list2.push_back(123);
+	list2.push_back(234);
+	list2.push_back(345);
+	list2.print();
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,9 +1,16 @@
 ﻿https://github.com/okovtun/Zazo.git
+https://www.youtube.com/watch?v=yghczE8EG-4&list=PLeqyOOqxeiIOQquR1jOH5H6gQ6HM41Etc&index=1
+
+https://meet.google.com/mho-khdw-fhj
 
 TODO:
 В класс ForwardList добавить методы:
-void pop_front();	//Удаляет элемент с начала списка
-void pop_back();	//Удаляет элемент с конца списка
+void pop_front();	//Удаляет элемент с начала списка			DONE
+void pop_back();	//Удаляет элемент с конца списка			DONE
 
-void insert(???);	//Вставляет элемент по указанному индексу
+void insert(???);	//Вставляет элемент по указанному индексу	DONE
 void erase(???);	//Удаляет элемент по указанному индексу
+
+Деструктор, который удаляет все элементы из списка;
+CopyConstructor - создаваемый объект делает точной копией существующего объекта
+CopyAssignment (Оператор присваивания) - существующий объект делает точной копией другого существующего объекта
